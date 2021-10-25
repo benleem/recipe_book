88f61d5b8740b855f91a7ff9555038ae7ee357c7
Ben Marshall <benleem98@yahoo.com>
2021-10-24 21:09:28 -0400

    called fethData function in other functions to add dynamic reloads

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
     addPost();
+    fetchData();
 })
 
 const fetchData = async () => {
@@ -74,6 +75,7 @@ const deletePost = () =>{
                     id: postId
                 })
             });
+            fetchData();
         });
     }
 }
